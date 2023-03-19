cca168d7443dd191f3a8ec264b87a3b768a69f99
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 12:05:04 +0000

    Update Refrigerant_3.py

--- Refrigerant_3.py
+++ Refrigerant_3.py
@@ -1,4 +1,4 @@
-#pip install streamlit CoolProp modelicares
+"""#pip install streamlit CoolProp modelicares
 
 import inspect
 import textwrap
@@ -36,3 +36,22 @@ ph_data = mr.plot_ph('R134a', P_min, P_max, T_min, T_max)
 
 # Display the PH diagram on Streamlit
 st.pyplot(ph_data.plot())
+"""
+
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import modelicares as mr
+
+# Load the ModelicaRes library
+mr.load_library("Modelica.Media.Water")
+
+# Define a range of temperatures to plot
+T_range = np.linspace(273.15, 373.15, 101)
+
+# Calculate the specific enthalpy and specific entropy of water at each temperature
+h = mr.ENTHALPY_WATER(T=T_range)
+s = mr.ENTROPY_WATER(T=T_range)
+
+# Create a Streamlit plot of specific enthalpy vs. specific entropy
+st.pyplot(plt.plot(s, h))
