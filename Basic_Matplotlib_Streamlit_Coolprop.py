dc09fa68e6e08875bd7d3124481fa8f1b842627e
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 14:07:56 +0000

    Update Basic_Matplotlib_Streamlit_Coolprop.py

--- Basic_Matplotlib_Streamlit_Coolprop.py
+++ Basic_Matplotlib_Streamlit_Coolprop.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('agg')  # Set the backend for matplotlib to 'agg'
-import numpy as np
-import CoolProp.CoolProp as CP
-import CoolProp.Plots as CPP
-
 from CoolProp.Plots import PropertyPlot
 
 # Create a PropertyPlot instance for R290 refrigerant
@@ -18,9 +13,6 @@ plot.calc_isolines()
 plot.xlabel('Specific enthalpy (kJ/kg)')
 plot.ylabel('Pressure (bar)')
 plot.title('R290 Refrigerant PH Diagram')
-
-# Display the plot on terminal GUI
-# plot.show()
 
 # Display the plot in Streamlit
 st.pyplot(plot.figure)
