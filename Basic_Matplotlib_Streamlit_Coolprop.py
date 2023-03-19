c1a9ac77aa2d8c5bd63a1227184fdced4bfadb1f
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 13:46:39 +0000

    Update Basic_Matplotlib_Streamlit_Coolprop.py

--- Basic_Matplotlib_Streamlit_Coolprop.py
+++ Basic_Matplotlib_Streamlit_Coolprop.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from CoolProp.CoolProp import PropsSI
 
-from CoolProp.Plots import PropsPlot
+from CoolProp.CoolProp import PropsPlot
 
 ph_plot = PropsPlot('R410A', 'Ph')
 ph_plot.show()
