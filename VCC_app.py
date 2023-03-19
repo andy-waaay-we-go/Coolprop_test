f64da614da059c46d586ff6c7b928492ee3eba03
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 21:03:27 +0000

    Update VCC_app.py

--- VCC_app.py
+++ VCC_app.py
@@ -59,50 +59,6 @@ Mechanical_eff = st.slider(
 'Estimate Compressor Mechanical Efficiency',
      0, 100, 90)
 
-P_Cond = PropsSI('P','T',T_Cond_K,'Q',0,refrigerant)
-P_Cond_barA = P_Cond/100000
-
-D = PropsSI('D','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)
-
-mdot = (Compressor_speed*D*Volumetric_eff/100*(Compressor_disp/1000000))/60
-
-Compressor_Entropy_in = PropsSI('S','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)/1000
-
-Compressor_Entropy_out = Compressor_Entropy_in/(Isentropic_eff/100)
-
-Compressor_Temperature_out_est = PropsSI('T','P',P_Cond,'S',Compressor_Entropy_out*1000,refrigerant)
-
-H_Comp_out = PropsSI('H','T',Compressor_Temperature_out_est,'P',P_Cond,refrigerant)
-
-H_Cond_out = PropsSI('H','T',T_Cond_K - Subcool,'P',P_Cond,refrigerant)
-
-H_Evap_out = PropsSI('H','T',T_Evap_K + Superheat,'P',P_Evap,refrigerant)
-
-Q_Cond  =  mdot*(H_Comp_out-H_Cond_out)
-
-Q_Evap  =  mdot*(H_Evap_out-H_Cond_out)
-
-W_Comp  =  mdot*(H_Comp_out-H_Evap_out)/(Mechanical_eff/100)
-
-COP_heating = Q_Cond/W_Comp
-
-# Update default settings to show 2 decimal place
-pd.options.display.float_format = '{:.2f}'.format
-
-# Create dataframe
-df = pd.DataFrame({'Result Output': ['Refrigerant Choice', 'Evaporator Temperture', 'Superheat', 'Evaporator Pressure', 'Suction Line Pressure Drop', 
-                    'Condenser Temperature', 'Subcool','Condenser Pressure','Compressor Displacement','Volumetric Efficiency','Compressor Speed',
-                    'Suction Line Refrigerant Density','Refrigerant Massflow Rate','Isentropic Efficiency','Estimate of Compressor Temperature Out',                    
-                    'Condenser Power','Evaporator Power','Mechanical Efficiency','Compressor Work','COP Heating'],
-                   'Value': [refrigerant, T_Evap, Superheat, float("{:.2f}".format(P_Evap_barA)), Pressure_drop, T_Cond, Subcool,
-                    float("{:.2f}".format(P_Cond_barA)),Compressor_disp,Volumetric_eff,Compressor_speed,float("{:.2f}".format(D)),float("{:.4f}".format(mdot)),Isentropic_eff,float("{:.2f}".format(Compressor_Temperature_out_est-273.15)),
-                    float("{:.2f}".format(Q_Cond/1000)),float("{:.2f}".format(Q_Evap/1000)),Mechanical_eff,float("{:.2f}".format(W_Comp/1000)),float("{:.2f}".format(COP_heating))],
-                   'Units': ['-', '°C', 'K', 'BarA', 'kPa', '°C', 'K','BarA','cc','%',
-                   'RPM','kg/m\u00b3','kg/s','%','°C','kW','kW','%','kW','']}) 
-
-Results = df.astype(str)
-st.table(Results) 
-
 def vapor_compression_cycle_points(refrigerant, evap_temp=T_Evap, cond_temp=T_Cond, subcooling=Subcool, superheat=Superheat):
     evap_pressure = PropsSI('P', 'T', evap_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
     cond_pressure = PropsSI('P', 'T', cond_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
@@ -166,3 +122,47 @@ def plot_ph_diagram(refrigerant):
 
 ph_fig = plot_ph_diagram(refrigerant)
 st.pyplot(ph_fig)
+
+P_Cond = PropsSI('P','T',T_Cond_K,'Q',0,refrigerant)
+P_Cond_barA = P_Cond/100000
+
+D = PropsSI('D','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)
+
+mdot = (Compressor_speed*D*Volumetric_eff/100*(Compressor_disp/1000000))/60
+
+Compressor_Entropy_in = PropsSI('S','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)/1000
+
+Compressor_Entropy_out = Compressor_Entropy_in/(Isentropic_eff/100)
+
+Compressor_Temperature_out_est = PropsSI('T','P',P_Cond,'S',Compressor_Entropy_out*1000,refrigerant)
+
+H_Comp_out = PropsSI('H','T',Compressor_Temperature_out_est,'P',P_Cond,refrigerant)
+
+H_Cond_out = PropsSI('H','T',T_Cond_K - Subcool,'P',P_Cond,refrigerant)
+
+H_Evap_out = PropsSI('H','T',T_Evap_K + Superheat,'P',P_Evap,refrigerant)
+
+Q_Cond  =  mdot*(H_Comp_out-H_Cond_out)
+
+Q_Evap  =  mdot*(H_Evap_out-H_Cond_out)
+
+W_Comp  =  mdot*(H_Comp_out-H_Evap_out)/(Mechanical_eff/100)
+
+COP_heating = Q_Cond/W_Comp
+
+# Update default settings to show 2 decimal place
+pd.options.display.float_format = '{:.2f}'.format
+
+# Create dataframe
+df = pd.DataFrame({'Result Output': ['Refrigerant Choice', 'Evaporator Temperture', 'Superheat', 'Evaporator Pressure', 'Suction Line Pressure Drop', 
+                    'Condenser Temperature', 'Subcool','Condenser Pressure','Compressor Displacement','Volumetric Efficiency','Compressor Speed',
+                    'Suction Line Refrigerant Density','Refrigerant Massflow Rate','Isentropic Efficiency','Estimate of Compressor Temperature Out',                    
+                    'Condenser Power','Evaporator Power','Mechanical Efficiency','Compressor Work','COP Heating'],
+                   'Value': [refrigerant, T_Evap, Superheat, float("{:.2f}".format(P_Evap_barA)), Pressure_drop, T_Cond, Subcool,
+                    float("{:.2f}".format(P_Cond_barA)),Compressor_disp,Volumetric_eff,Compressor_speed,float("{:.2f}".format(D)),float("{:.4f}".format(mdot)),Isentropic_eff,float("{:.2f}".format(Compressor_Temperature_out_est-273.15)),
+                    float("{:.2f}".format(Q_Cond/1000)),float("{:.2f}".format(Q_Evap/1000)),Mechanical_eff,float("{:.2f}".format(W_Comp/1000)),float("{:.2f}".format(COP_heating))],
+                   'Units': ['-', '°C', 'K', 'BarA', 'kPa', '°C', 'K','BarA','cc','%',
+                   'RPM','kg/m\u00b3','kg/s','%','°C','kW','kW','%','kW','']}) 
+
+Results = df.astype(str)
+st.table(Results) 
