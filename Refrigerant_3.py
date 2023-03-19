1f43860d70ffbb72e64b1cfe91e959bd2fe5b41c
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 16:05:21 +0000

    Update Refrigerant_3.py

--- Refrigerant_3.py
+++ Refrigerant_3.py
@@ -6,20 +6,21 @@ import numpy as np
 import CoolProp.CoolProp as CP
 import CoolProp.Plots as CPP
 
-import CoolProp
 from CoolProp.Plots import PropertyPlot
 from CoolProp.Plots import SimpleCompressionCycle
+
 pp = PropertyPlot('HEOS::R134a', 'PH', unit_system='EUR')
 pp.calc_isolines(CoolProp.iQ, num=11)
 cycle = SimpleCompressionCycle('HEOS::R134a', 'PH', unit_system='EUR')
-Te = 265
-Tc = 300
-cycle.simple_solve_dt(Te, Tc, 10, 15, 0.7, SI=True)
+T0 = 280
+pp.state.update(CoolProp.QT_INPUTS,0.0,T0-15)
+p0 = pp.state.keyed_output(CoolProp.iP)
+T2 = 310
+pp.state.update(CoolProp.QT_INPUTS,1.0,T2+10)
+p2 = pp.state.keyed_output(CoolProp.iP)
+cycle.simple_solve(T0, p0, T2, p2, 0.7, SI=True)
 cycle.steps = 50
 sc = cycle.get_state_changes()
 import matplotlib.pyplot as plt
 plt.close(cycle.figure)
 pp.draw_process(sc)
-
-# Display the plot in Streamlit
-st.pyplot(plt.figure)
