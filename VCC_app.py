7bdbc0436ef66e847a2aeba6735ca8b385855d92
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-19 22:19:18 +0000

    Update VCC_app.py

--- VCC_app.py
+++ VCC_app.py
@@ -105,8 +105,8 @@ def plot_ph_diagram(refrigerant):
 
     # Create the p-h diagram
     plt.figure(figsize=(10, 6))
-    contour_plot = plt.contour(H, P, T, levels=temperature_levels, cmap="coolwarm")
-    plt.clabel(contour_plot, inline=1, fontsize=8, fmt='%1.0f')  # Add labels to the contour lines
+    #contour_plot = plt.contour(H, P, T, levels=temperature_levels, cmap="coolwarm")
+    #plt.clabel(contour_plot, inline=1, fontsize=8, fmt='%1.0f')  # Add labels to the contour lines
     plt.plot(h_sat_liq, [PropsSI('P', 'T', T, 'Q', 0, refrigerant) / 1e5 for T in T_sat], 'k--', label="Saturation Liquid")
     plt.plot(h_sat_vap, [PropsSI('P', 'T', T, 'Q', 1, refrigerant) / 1e5 for T in T_sat], 'k-.', label="Saturation Vapor")
    
