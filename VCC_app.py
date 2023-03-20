a364a84042974782357c4a973fa9536570b53c28
andy-waaay-we-go <andrewmccahey1@gmail.com>
2023-03-20 13:10:21 +0000

    Update VCC_app.py

--- VCC_app.py
+++ VCC_app.py
@@ -15,59 +15,47 @@ st.title("Pressure-Enthalpy Diagram")
 
 refrigerant = st.selectbox("Select refrigerant:", ["R134a","R1234yf", "R290", "R600a", "R410A", "R32", "R404a"])
 
-def plot_ph_diagram(refrigerant):
-    # Define pressure and enthalpy ranges
-    T_min = T_Evap_K - 10
-    T_max = T_Cond_K + 5
-    p_min = PropsSI('P', 'T', T_min, 'Q', 0, refrigerant) / 1e5  # bar
-    p_max = PropsSI('P', 'T', T_max, 'Q', 0, refrigerant) / 1e5  # bar
-    h_min = PropsSI('H', 'T', T_min, 'Q', 0, refrigerant) / 1e3  # kJ/kg
-    h_max = PropsSI('H', 'T', T_max, 'Q', 1, refrigerant) / 1e3  # kJ/kg
+P_Cond = PropsSI('P','T',T_Cond_K,'Q',0,refrigerant)
 
-    pressures = np.linspace(p_min, p_max, 200)
-    enthalpies = np.linspace(h_min, h_max, 200)
+P_Cond_barA = P_Cond/100000
 
-    H, P = np.meshgrid(enthalpies, pressures)
+Compressor_Entropy_in = PropsSI('S','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)/1000
 
-    # Calculate saturation lines
-    T_sat = np.linspace(T_min, T_max, 100)
-    h_sat_liq = [PropsSI('H', 'T', T, 'Q', 0, refrigerant) / 1e3 for T in T_sat]
-    h_sat_vap = [PropsSI('H', 'T', T, 'Q', 1, refrigerant) / 1e3 for T in T_sat]
+Compressor_Entropy_out = Compressor_Entropy_in/(Isentropic_eff/100)
 
-    # Calculate temperature grid
-    T = np.zeros_like(H)
-    for i, p in enumerate(pressures):
-        for j, h in enumerate(enthalpies):
-            try:
-                T[i, j] = PropsSI('T', 'P', p * 1e5, 'H', h * 1e3, refrigerant) - 273.15  # °C
-            except ValueError:
-                T[i, j] = np.nan
+Compressor_Temperature_out_est = PropsSI('T','P',P_Cond,'S',Compressor_Entropy_out*1000,refrigerant)
 
-    # Create the p-h diagram
-    plt.figure(figsize=(10, 6))
-    contour_plot = plt.contour(H, P, T, levels=20, cmap="coolwarm")
-    plt.clabel(contour_plot, inline=1, fontsize=8, fmt='%1.0f')  # Add labels to the contour lines
-    plt.plot(h_sat_liq, [PropsSI('P', 'T', T, 'Q', 0, refrigerant) / 1e5 for T in T_sat], 'k--', label="Saturation Liquid")
-    plt.plot(h_sat_vap, [PropsSI('P', 'T', T, 'Q', 1, refrigerant) / 1e5 for T in T_sat], 'k-.', label="Saturation Vapor")
+H_Comp_out = PropsSI('H','T',Compressor_Temperature_out_est,'P',P_Cond,refrigerant)
 
-    # Get the vapor compression cycle points
-    points = vapor_compression_cycle_points(refrigerant)
+H_Cond_out = PropsSI('H','T',T_Cond_K - Subcool,'P',P_Cond,refrigerant)
 
-    # Add the vapor compression cycle points to the plot
-    plt.plot([points[0][0], points[1][0]], [points[0][1], points[1][1]], 'go-') #label="Compression"
-    plt.plot([points[1][0], points[2][0]], [points[1][1], points[2][1]], 'go-')
-    plt.plot([points[2][0], points[3][0]], [points[2][1], points[3][1]], 'go-')
-    plt.plot([points[3][0], points[0][0]], [points[3][1], points[0][1]], 'go-')
+H_Evap_out = PropsSI('H','T',T_Evap_K + Superheat,'P',P_Evap,refrigerant)
 
-    plt.xlabel('Enthalpy [kJ/kg]')
-    plt.ylabel('Pressure [bar]')
-    plt.title(f'Pressure-Enthalpy Diagram for {refrigerant}')
-    plt.legend()
-    plt.grid()
-    return plt.gcf()
+D = PropsSI('D','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)
 
-ph_fig = plot_ph_diagram(refrigerant)
-st.pyplot(ph_fig)
+mdot = (Compressor_speed*D*Volumetric_eff/100*(Compressor_disp/1000000))/60
+
+Q_Cond  =  mdot*(H_Comp_out-H_Cond_out)
+
+Q_Evap  =  mdot*(H_Evap_out-H_Cond_out)
+
+W_Comp  =  mdot*(H_Comp_out-H_Evap_out)/(Mechanical_eff/100)
+
+COP_heating = Q_Cond/W_Comp
+
+
+def vapor_compression_cycle_points(refrigerant, evap_temp=T_Evap, cond_temp=T_Cond, subcooling=Subcool, superheat=Superheat, H_Comp_out=H_Comp_out, H_Cond_out=H_Cond_out, H_Evap_out=H_Evap_out):
+   
+    evap_pressure = PropsSI('P', 'T', evap_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
+
+    cond_pressure = PropsSI('P', 'T', cond_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
+
+    h1 = H_Evap_out / 1e3  # kJ/kg
+    h2 = H_Comp_out / 1e3  # kJ/kg
+    h3 = H_Cond_out / 1e3  # kJ/kg
+    h4 = h3 # kJ/kg
+
+    return (h1, evap_pressure), (h2, cond_pressure), (h3, cond_pressure), (h4, evap_pressure)
 
 T_Evap = st.slider(
 'Select Evaporator Temperature in °C',
@@ -110,48 +98,6 @@ Isentropic_eff = st.slider(
 Mechanical_eff = st.slider(
 'Estimate Compressor Mechanical Efficiency',
     0, 100, 90)
-
-P_Cond = PropsSI('P','T',T_Cond_K,'Q',0,refrigerant)
-
-P_Cond_barA = P_Cond/100000
-
-Compressor_Entropy_in = PropsSI('S','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)/1000
-
-Compressor_Entropy_out = Compressor_Entropy_in/(Isentropic_eff/100)
-
-Compressor_Temperature_out_est = PropsSI('T','P',P_Cond,'S',Compressor_Entropy_out*1000,refrigerant)
-
-H_Comp_out = PropsSI('H','T',Compressor_Temperature_out_est,'P',P_Cond,refrigerant)
-
-H_Cond_out = PropsSI('H','T',T_Cond_K - Subcool,'P',P_Cond,refrigerant)
-
-H_Evap_out = PropsSI('H','T',T_Evap_K + Superheat,'P',P_Evap,refrigerant)
-
-D = PropsSI('D','T',T_Evap_K + Superheat,'P',P_Evap-Pdrop_kPa,refrigerant)
-
-mdot = (Compressor_speed*D*Volumetric_eff/100*(Compressor_disp/1000000))/60
-
-Q_Cond  =  mdot*(H_Comp_out-H_Cond_out)
-
-Q_Evap  =  mdot*(H_Evap_out-H_Cond_out)
-
-W_Comp  =  mdot*(H_Comp_out-H_Evap_out)/(Mechanical_eff/100)
-
-COP_heating = Q_Cond/W_Comp
-
-
-def vapor_compression_cycle_points(refrigerant, evap_temp=T_Evap, cond_temp=T_Cond, subcooling=Subcool, superheat=Superheat, H_Comp_out=H_Comp_out, H_Cond_out=H_Cond_out, H_Evap_out=H_Evap_out):
-   
-    evap_pressure = PropsSI('P', 'T', evap_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
-
-    cond_pressure = PropsSI('P', 'T', cond_temp + 273.15, 'Q', 0, refrigerant) / 1e5  # bar
-
-    h1 = H_Evap_out / 1e3  # kJ/kg
-    h2 = H_Comp_out / 1e3  # kJ/kg
-    h3 = H_Cond_out / 1e3  # kJ/kg
-    h4 = h3 # kJ/kg
-
-    return (h1, evap_pressure), (h2, cond_pressure), (h3, cond_pressure), (h4, evap_pressure)
 
 # Update default settings to show 2 decimal place
 pd.options.display.float_format = '{:.2f}'.format
